96c0d9a7a32f12d3b8e9312930b8d2c8434e5c7b
johel cueva <johel.cueva@gmail.com>
2025-06-21 13:08:46 -0500

    corrigiendo otro

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,6 @@
 import { ProductCard } from '@/components/ProductCard'
 import { prisma } from '@/lib/db/prisma'
-
+import  { type Product } from '@prisma/client'
 
 export default async function CatalogPage() {
   const products = await prisma.product.findMany()
@@ -12,10 +12,9 @@ export default async function CatalogPage() {
           Catálogo
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {products.map((product) => (
-            <div key={product.id} className="...">
+          {products.map((product: Product) => (
+            <div key={product.id}>
               <ProductCard {...product} />
-              
             </div>
           ))}
         </div>
